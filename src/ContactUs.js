4c648b30a5b2c068456dfb113970ea2fe7c29113
ParasArya289 <parasarya289@gmail.com>
2023-01-17 00:39:32 +0530

    fix: changed then chaining to async function

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,55 +1,60 @@
-import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export const ContactUs = () => {
   const form = useRef();
-  const [otp,setotp]=useState("");
+  const [otp, setotp] = useState("");
 
-  const otpp = () =>{
+  const generateOtp = () => {
     setotp(Math.floor(1000 + Math.random() * 9000));
-    
-  }
-
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    otpp();
-    console.log(otp);
-    // setotp(Math.floor(1000 + Math.random() * 9000));
-   
-
-    emailjs.sendForm('service_5ijhdv4', 'template_f5jb4wq', form.current, 'dw0IdgTXXNcUc6QWE')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
   };
+
+  const sendEmail = async(e) => {
+    e.preventDefault();
+    try{
+      generateOtp();
+      let res = await emailjs.sendForm('service_5ijhdv4', 'template_f5jb4wq', form.current, 'dw0IdgTXXNcUc6QWE');
+      if(!res){
+        throw new Error(`something went wrong ${res.status}`);
+      }
+      let data = res.text;
+      console.log(data);
+
+    }catch(err){
+      console.error(err.message)
+    }
+
+    // emailjs.sendForm('service_5ijhdv4', 'template_f5jb4wq', form.current, 'dw0IdgTXXNcUc6QWE')
+    //   .then((result) => {
+    //       console.log(result.text);
+    //   }, (error) => {
+    //       console.log(error.text);
+    //   });
+
+
+  };
+  console.log(otp);
 
   return (
     <>
-    {otp ? (<h1> otp box </h1>):(
-    <div>  
-    <form ref={form} onSubmit={sendEmail}>
-    <label>Name</label>
-    <input type="text" name="user_name" />
-    <label>Email</label>
-    <input type="email" name="user_email" />
-    <label>Message</label>
-    <textarea name="message" />
+      {otp ? (
+        <h1> otp box </h1>
+      ) : (
+        <div>
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
 
-    <input type="hidden"
-    name='otp'
-    value={otp}/>
-    
-    <input type="submit" value="Send" />
-    </form>
-  </div>
-    ) 
-}
-  </>
-  )
+            <input type="hidden" name="otp" value={otp} />
+
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      )}
+    </>
+  );
 };
-   
-  
